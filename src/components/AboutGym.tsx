9c61f02043b9c349193c83e887932fb7077f787d
Arsen Margaryan <arsen_margaryan@epam.com>
2025-03-18 14:36:00 +0400

    fix

--- src/components/AboutGym.tsx
+++ src/components/AboutGym.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function AboutGym() {
   return (
     <div className=" m-3 mt-15 sm:m-10 sm:mt-20 md:m-30 xl:m-30 2xl:m-55 object-contain">
-                <div className="text-3xl">ABOUT OUR GYM</div>
+                <div className="text-3xl mb-3">ABOUT OUR GYM</div>
                 <div className=" object-contain">
                   
                 </div>
